a5444d0cc2bdf3cc3ed2b69a11ca2819ee5dbe6d
Papajohn77 <t8190314@aueb.gr>
2025-08-26 00:07:49 +0300

    Detect JPA relationships between the selected Entities and the rest of the Entities, display them in the TableViewer, and display error if there is an aggregate violation

--- src/gr/aueb/java/archifactor/refactoring/views/UnmapJpaRelationships.java
+++ src/gr/aueb/java/archifactor/refactoring/views/UnmapJpaRelationships.java
@@ -12,6 +12,15 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import java.lang.reflect.InvocationTargetException;
+import org.eclipse.ui.progress.IProgressService;
+import org.eclipse.swt.widgets.Display;
+import gr.uom.java.ast.ASTReader;
+import gr.uom.java.ast.CompilationErrorDetectedException;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.ui.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -23,7 +32,6 @@ import org.eclipse.ui.dialogs.CheckedTreeSelectionDialog;
 import org.eclipse.jdt.ui.JavaElementLabelProvider;
 import gr.uom.java.ast.SystemObject;
 import gr.uom.java.ast.ClassObject;
-import gr.aueb.java.ddd.aggregatesIdentification.SystemObjectProvider;
 import gr.aueb.java.jpa.JpaModel;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
@@ -33,9 +41,17 @@ import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.IAnnotation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.NormalAnnotation;
+import org.eclipse.jdt.core.dom.MemberValuePair;
+import gr.uom.java.ast.FieldObject;
 
 
 public class UnmapJpaRelationships extends ViewPart {
@@ -45,7 +61,45 @@ public class UnmapJpaRelationships extends ViewPart {
 	private Action doubleClickAction;
 	private ComboViewer projectComboViewer;
 	private IJavaProject selectedProject;
+	private SystemObject cachedSystemObject;
 	private List<ClassObject> selectedEntities = new ArrayList<ClassObject>();
+	private List<RelationshipInfo> detectedRelationships = new ArrayList<RelationshipInfo>();
+
+	class AggregateViolationException extends RuntimeException {
+		public AggregateViolationException(String message) {
+			super(message);
+		}
+	}
+
+	class RelationshipInfo {
+		private String fromEntity;
+		private String relationshipType;
+		private String toEntity;
+		private boolean isOwningSide;
+		
+		public RelationshipInfo(String fromEntity, String relationshipType, String toEntity, boolean isOwningSide) {
+			this.fromEntity = fromEntity;
+			this.relationshipType = relationshipType;
+			this.toEntity = toEntity;
+			this.isOwningSide = isOwningSide;
+		}
+		
+		public String getFromEntity() {
+			return fromEntity;
+		}
+
+		public String getRelationshipType() {
+			return relationshipType;
+		}
+
+		public String getToEntity() {
+			return toEntity;
+		}
+		
+		public boolean isOwningSide() {
+			return isOwningSide;
+		}
+	}
 
 	class ViewContentProvider implements IStructuredContentProvider {
 		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
@@ -53,24 +107,26 @@ public class UnmapJpaRelationships extends ViewPart {
 		public void dispose() {
 		}
 		public Object[] getElements(Object parent) {
-			// Return empty array for now - no data to populate
-			return new Object[] {};
+			return detectedRelationships.toArray();
 		}
 	}
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		public String getColumnText(Object obj, int index) {
-			// Return empty strings for now - no data to display
-			switch(index) {
-			case 0:
-				return ""; // From Entity
-			case 1:
-				return ""; // Relationship
-			case 2:
-				return ""; // To Entity
-			default:
-				return "";
+			if (obj instanceof RelationshipInfo) {
+				RelationshipInfo rel = (RelationshipInfo) obj;
+				switch(index) {
+				case 0:
+					return rel.getFromEntity();
+				case 1:
+					return rel.getRelationshipType();
+				case 2:
+					return rel.getToEntity();
+				default:
+					return "";
+				}
 			}
+			return "";
 		}
 		public Image getColumnImage(Object obj, int index) {
 			return null;
@@ -171,33 +227,48 @@ public class UnmapJpaRelationships extends ViewPart {
 		tableViewer.setLabelProvider(new ViewLabelProvider());
 		tableViewer.setSorter(new NameSorter());
 		tableViewer.setInput(getViewSite());
+
+		TableColumn fromEntityColumn = new TableColumn(tableViewer.getTable(), SWT.LEFT);
+		fromEntityColumn.setText("From Entity");
+		fromEntityColumn.setWidth(200);
 		
+		TableColumn relationshipColumn = new TableColumn(tableViewer.getTable(), SWT.LEFT);
+		relationshipColumn.setText("Relationship");
+		relationshipColumn.setWidth(150);
+		
+		TableColumn toEntityColumn = new TableColumn(tableViewer.getTable(), SWT.LEFT);
+		toEntityColumn.setText("To Entity");
+		toEntityColumn.setWidth(200);
+
 		TableLayout tableLayout = new TableLayout();
-		tableLayout.addColumnData(new ColumnWeightData(33, true));
-		tableLayout.addColumnData(new ColumnWeightData(34, true));
-		tableLayout.addColumnData(new ColumnWeightData(33, true));
+		tableLayout.addColumnData(new ColumnWeightData(40, 150));
+		tableLayout.addColumnData(new ColumnWeightData(20, 100));
+		tableLayout.addColumnData(new ColumnWeightData(40, 150));
 		tableViewer.getTable().setLayout(tableLayout);
-		
-		new TableColumn(tableViewer.getTable(), SWT.LEFT).setText("From Entity");
-		new TableColumn(tableViewer.getTable(), SWT.LEFT).setText("Relationship");
-		new TableColumn(tableViewer.getTable(), SWT.LEFT).setText("To Entity");
-		
+
 		tableViewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
 				// Selection changed - could be used for future functionality
 			}
 		});
 
-		for (int i = 0, n = tableViewer.getTable().getColumnCount(); i < n; i++) {
-			tableViewer.getTable().getColumn(i).pack();
-		}
-
 		tableViewer.getTable().setLinesVisible(true);
 		tableViewer.getTable().setHeaderVisible(true);
 		makeActions();
 		hookDoubleClickAction();
 		contributeToActionBars();
-		// Removed selection listeners for simplified layout
+
+		// Do the heavy-lifting of creating the SystemObject in the beginning
+		if (!projectComboViewer.getSelection().isEmpty()) {
+			onProjectSelectedBuildSystemObject();
+		}
+
+		projectComboViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				onProjectSelectedBuildSystemObject();
+			}
+		});
 	}
 
 	private void contributeToActionBars() {
@@ -257,28 +328,206 @@ public class UnmapJpaRelationships extends ViewPart {
 		// Removed selection listener cleanup for simplified layout
 	}
 	
+	private void onProjectSelectedBuildSystemObject() {
+		Shell shell = getSite().getShell();
+		
+		IStructuredSelection selection = (IStructuredSelection) projectComboViewer.getSelection();
+		IJavaProject project = (IJavaProject) selection.getFirstElement();
+		if (selectedProject != null && selectedProject.equals(project)) {
+			return;
+		}
+		
+		selectedProject = project;
+		selectedEntities.clear();
+		detectedRelationships.clear();
+		tableViewer.refresh();
+
+		try {
+			IWorkbench wb = PlatformUI.getWorkbench();
+			IProgressService ps = wb.getProgressService();
+			if (ASTReader.getSystemObject() != null && project.equals(ASTReader.getExaminedProject())) {
+				new ASTReader(project, ASTReader.getSystemObject(), null);
+			} else {
+				ps.busyCursorWhile(new IRunnableWithProgress() {
+					public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+						try {
+							new ASTReader(project, monitor);
+						} catch (CompilationErrorDetectedException e) {
+							Display.getDefault().asyncExec(new Runnable() {
+								public void run() {
+									MessageDialog.openInformation(shell, "Compilation Errors", 
+										"Compilation errors were detected in the project. Fix the errors before using this feature.");
+								}
+							});
+						}
+					}
+				});
+			}
+			cachedSystemObject = ASTReader.getSystemObject();
+		} catch (InterruptedException e) {
+			cachedSystemObject = null;
+		} catch (InvocationTargetException e) {
+			cachedSystemObject = null;
+			MessageDialog.openError(shell, "Error Loading Project", 
+				"Error loading project structure: " + e.getTargetException().getMessage());
+		} catch (CompilationErrorDetectedException e) {
+			cachedSystemObject = null;
+			MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Compilation Errors",
+				"Compilation errors were detected in the project. Fix the errors before using JDeodorant.");
+		}
+	}
+
 	private void selectEntities() {
 		Shell shell = getSite().getShell();
 
-		IStructuredSelection selection = (IStructuredSelection) projectComboViewer.getSelection();
-		if (selection.isEmpty()) {
-			MessageDialog.openInformation(shell, "No Project Selected", "Please select a project first.");
+		if (selectedProject == null || cachedSystemObject == null) {
+			MessageDialog.openError(shell, "Project Not Loaded", "Project structure is not loaded. Please reselect the project and try again.");
 			return;
 		}
 
-		IJavaProject project = (IJavaProject) selection.getFirstElement();
-		selectedProject = project;
-
-		List<ClassObject> entities = selectEntityClasses(shell, project);
+		List<ClassObject> entities = selectEntityClasses(shell, selectedProject);
 		if (entities != null && !entities.isEmpty()) {
 			selectedEntities = entities;
-			for (ClassObject entity : selectedEntities) {
-				System.out.println(entity.getName());
+			
+			try {
+				detectRelationships();
+				tableViewer.refresh();
+			} catch (AggregateViolationException e) {
+				MessageDialog.openError(shell, "Cannot Break Relationships", 
+					"Cannot proceed with breaking relationships:\n" + e.getMessage() + 
+					"\n\nThese properties indicate that the entities belong to the same aggregate and should not be separated.");
+			} catch (Exception e) {
+				MessageDialog.openError(shell, "Error", "Error analyzing relationships:\n" + e.getMessage());
 			}
-			// TODO: Update the table viewer to show selected entities
 		}
 	}
 	
+	private void detectRelationships() {
+		detectedRelationships.clear();
+		
+		Set<String> allEntityNames = new HashSet<String>();
+		ListIterator<ClassObject> classIterator = cachedSystemObject.getClassListIterator();
+		while (classIterator.hasNext()) {
+			ClassObject classObj = classIterator.next();
+			if (JpaModel.isEntity(classObj)) {
+				allEntityNames.add(classObj.getName());
+			}
+		}
+
+		Set<String> selectedEntityNames = selectedEntities.stream()
+			.map(ClassObject::getName)
+			.collect(Collectors.toSet());
+
+		Set<String> otherEntityNames = new HashSet<String>(allEntityNames);
+		otherEntityNames.removeAll(selectedEntityNames);
+		
+		// 1. Detect relationships FROM selected entities TO other entities
+		for (ClassObject selectedEntity : selectedEntities) {
+			Iterator<FieldObject> fieldIterator = selectedEntity.getFieldIterator();
+			while (fieldIterator.hasNext()) {
+				// private Customer customer;  // name="customer", type.classType="Customer"
+				// private List<Item> items;   // name="items", type.classType="List", type.genericType="<Item>"
+				FieldObject fieldObject = fieldIterator.next();
+				String fieldTypeName = fieldObject.getType().getClassType();
+				String genericType = fieldObject.getType().getGenericType();
+				if (genericType != null) {
+					fieldTypeName = genericType.replaceAll("[<>]", "").trim();
+				}
+
+				if (otherEntityNames.contains(fieldTypeName)) {
+					boolean isOwningSide = false;
+					String relationshipType = null;
+					for (Annotation annotation : fieldObject.getAnnotations()) {
+						String annotationType = annotation.getTypeName().getFullyQualifiedName();
+						if (isJpaRelationshipAnnotation(annotationType)) {
+							relationshipType = annotationType;
+							checkForDangerousCascading(selectedEntity, fieldObject, annotation);
+						} else if (isJoinAnnotation(annotationType)) {
+							isOwningSide = true;
+						}
+					}
+
+					if (relationshipType != null) {
+						detectedRelationships.add(
+							new RelationshipInfo(selectedEntity.getName(), relationshipType, fieldTypeName, isOwningSide));
+					}
+				}
+			}
+		}
+		
+		// 2. Detect relationships FROM other entities TO selected entities
+		ListIterator<ClassObject> allClassIterator = cachedSystemObject.getClassListIterator();
+		while (allClassIterator.hasNext()) {
+			ClassObject otherEntity = allClassIterator.next();
+			if (selectedEntityNames.contains(otherEntity.getName()) || !JpaModel.isEntity(otherEntity)) {
+				continue;
+			}
+			
+			Iterator<FieldObject> fieldIterator = otherEntity.getFieldIterator();
+			while (fieldIterator.hasNext()) {
+				// private Customer customer;  // name="customer", type.classType="Customer"
+				// private List<Item> items;   // name="items", type.classType="List", type.genericType="<Item>"
+				FieldObject fieldObject = fieldIterator.next();
+				String fieldTypeName = fieldObject.getType().getClassType();
+				String genericType = fieldObject.getType().getGenericType();
+				if (genericType != null) {
+					fieldTypeName = genericType.replaceAll("[<>]", "").trim();
+				}
+
+				if (selectedEntityNames.contains(fieldTypeName)) {
+					boolean isOwningSide = false;
+					String relationshipType = null;
+					for (Annotation annotation : fieldObject.getAnnotations()) {
+						String annotationType = annotation.getTypeName().getFullyQualifiedName();
+						if (isJpaRelationshipAnnotation(annotationType)) {
+							relationshipType = annotationType;
+							checkForDangerousCascading(otherEntity, fieldObject, annotation);
+						} else if (isJoinAnnotation(annotationType)) {
+							isOwningSide = true;
+						}
+					}
+
+					if (relationshipType != null) {
+						detectedRelationships.add(
+							new RelationshipInfo(otherEntity.getName(), relationshipType, fieldTypeName, isOwningSide));
+					}
+				}
+			}
+		}
+	}
+
+	private boolean isJpaRelationshipAnnotation(String annotationType) {
+		return annotationType.equals("OneToMany") || 
+			   annotationType.equals("ManyToOne") || 
+			   annotationType.equals("OneToOne") || 
+			   annotationType.equals("ManyToMany");
+	}
+
+	private boolean isJoinAnnotation(String annotationType) {
+		return annotationType.equals("JoinColumn") || 
+			   annotationType.equals("JoinTable");
+	}
+
+	private void checkForDangerousCascading(ClassObject entity, FieldObject field, Annotation annotation) {
+		if (annotation instanceof NormalAnnotation) {
+			NormalAnnotation normalAnnotation = (NormalAnnotation) annotation;
+			for (Object obj : normalAnnotation.values()) {
+				MemberValuePair pair = (MemberValuePair) obj;
+				String propertyName = pair.getName().getIdentifier();
+				String propertyValue = pair.getValue().toString();
+				if (propertyName.equals("cascade")) {
+					if (propertyValue.contains("CascadeType.PERSIST") || propertyValue.contains("CascadeType.ALL")) {
+						throw new AggregateViolationException("Entity " + entity.getName() + "." + field.getName() + " has: " + propertyName + " = " + propertyValue);
+					}
+				} else if (propertyName.equals("orphanRemoval")) {
+					if (propertyValue.equals("true")) {
+						throw new AggregateViolationException("Entity " + entity.getName() + "." + field.getName() + " has: orphanRemoval=true");
+					}
+				}
+			}
+		}
+	}
+
 	private void populateProjectCombo() {
 		try {
 			List<IJavaProject> projects = new ArrayList<IJavaProject>();
@@ -319,19 +568,11 @@ public class UnmapJpaRelationships extends ViewPart {
 
 			if (dialog.open() == Window.OK) {
 				Object[] result = dialog.getResult();
-
-				// Now convert selected ICompilationUnits to ClassObjects using SystemObject
-				SystemObject systemObject = SystemObjectProvider.getSystemObject(project);
-				if (systemObject == null) {
-					MessageDialog.openError(shell, "Error", "Could not analyze project structure for selected entities.");
-					return null;
-				}
-
 				List<ClassObject> selectedEntities = new ArrayList<ClassObject>();
 				for (Object obj : result) {
 					if (obj instanceof ICompilationUnit) {
 						ICompilationUnit cu = (ICompilationUnit) obj;
-						Set<ClassObject> classObjs = systemObject.getClassObjects(cu);
+						Set<ClassObject> classObjs = cachedSystemObject.getClassObjects(cu);
 						for (ClassObject classObj : classObjs) {
 							if (JpaModel.isEntity(classObj)) {
 								selectedEntities.add(classObj);
